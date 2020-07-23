523cc94ddb97c7e4afc3cb9c857f2c68d04b1f98
phen0menon <garaganda21@gmail.com>
2020-07-23 17:07:38 +0300

    Remove all unnecessary testing code

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,34 +3,22 @@ import withAuthentication, {
   AuthenticationStatus,
 } from 'hocs/withAuthentication';
 import useAction from 'hooks/useAction';
-import { sessionActions } from 'models/session/slice';
-import useModal from 'hooks/useModal';
-import Modal from 'components/Modal';
-import Button from 'components/Button';
-
-const TestModal = ({ ...restProps }) => {
-  return (
-    <Modal {...restProps}>
-      <Modal.Header>qweq</Modal.Header>
-      <Modal.Body>Body this kis !</Modal.Body>
-      <Modal.Footer></Modal.Footer>
-    </Modal>
-  );
-};
+import { quizzesActions } from 'models/quizzes/slice';
+import QuizList from './QuizList';
 
 const Home = () => {
-  const onFetchLogout = useAction(sessionActions.fetchLogout);
+  const onFetchQuizzes = useAction(quizzesActions.fetchQuizzes);
 
-  const { isOpen, closeModal, openModal } = useModal();
+  React.useEffect(() => {
+    onFetchQuizzes();
+  }, []);
 
   return (
     <div>
-      Home
-      <Button onClick={onFetchLogout}>Logout</Button>
-      <Button onClick={openModal}>Open modal...</Button>
-      <TestModal isOpen={isOpen} close={closeModal} open={openModal} />
+      Quizer
+      <QuizList />
     </div>
   );
 };
 
-export default withAuthentication(AuthenticationStatus.AUTHENTICATED)(Home);
+export default withAuthentication(AuthenticationStatus.ANY)(Home);
