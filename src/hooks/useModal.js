a90c519d442dde30a6556f1caf9d6ed34964eff5
phen0menon <garaganda21@gmail.com>
2020-07-31 13:39:15 +0300

    Rename useModal props in order to simplicity passing props

--- src/hooks/useModal.js
+++ src/hooks/useModal.js
@@ -2,15 +2,15 @@ import { useState, useCallback } from 'react';
 
 export default () => {
   const [isOpen, setOpen] = useState(false);
-  const toggleModal = useCallback(() => setOpen(!isOpen), [isOpen, setOpen]);
-  const openModal = useCallback(() => setOpen(true), [setOpen]);
-  const closeModal = useCallback(() => setOpen(false), [setOpen]);
+  const toggle = useCallback(() => setOpen(!isOpen), [isOpen, setOpen]);
+  const open = useCallback(() => setOpen(true), [setOpen]);
+  const close = useCallback(() => setOpen(false), [setOpen]);
 
   return {
     isOpen,
     setOpen,
-    toggleModal,
-    openModal,
-    closeModal,
+    toggle,
+    open,
+    close,
   };
 };
