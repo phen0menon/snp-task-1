8e738ef2745fd168235e91a433a98f54ca6cd09b
phen0menon <garaganda21@gmail.com>
2020-08-17 11:36:27 +0300

    EditQuiz: Fix the dependency warning of useEffect

--- src/pages/Quiz/EditQuiz/EditQuiz.jsx
+++ src/pages/Quiz/EditQuiz/EditQuiz.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 
 import useSelector from 'hooks/useSelector';
 import useAction from 'hooks/useAction';
+import useVariable from 'hooks/useVariable';
 
 import {
   getQuestionsByIdsSelector,
@@ -13,21 +14,30 @@ import { questionsActions } from 'models/tests/questions/slice';
 import QuestionsSidebar from 'components/QuestionsSidebar/QuestionsSidebar';
 import QuizQuestion from '../components/QuizQuestion';
 
-import styles from './EditQuiz.scss';
 import withAuthentication, {
   AuthenticationStatus,
 } from 'hocs/withAuthentication';
+
+import styles from './EditQuiz.scss';
 
 const EditQuiz = ({ questions, title }) => {
   const questionId = useSelector(getCurrentQuestionIdSelector);
   const questionList = useSelector(getQuestionsByIdsSelector, questions);
   const onQuestionOpen = useAction(questionsActions.openQuestion);
 
+  const [redirectedAfterRender, setRedirectedAfterRender] = useVariable(false);
+
   React.useEffect(() => {
-    if (questions.length) {
+    if (questions.length && !redirectedAfterRender) {
       onQuestionOpen({ id: questions[0] });
+      setRedirectedAfterRender(true);
     }
-  });
+  }, [
+    redirectedAfterRender,
+    questions,
+    onQuestionOpen,
+    setRedirectedAfterRender,
+  ]);
 
   return (
     <div className={styles.root}>
